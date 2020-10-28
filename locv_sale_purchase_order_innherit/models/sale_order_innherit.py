1eaa6a9eb3372305c51ca7a220852c5aba1b964b
Jose Carmona <joseisaiascarmonahernandez@gmail.com>
2020-10-28 12:37:36 -0400

    Add files via upload

---
+++ locv_sale_purchase_order_innherit/models/sale_order_innherit.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+##############################################################################
+
+
+import ast
+import datetime, time
+
+from odoo.tools.translate import _
+from odoo import models, fields, api
+#
+
+
+
+class SaleOrderInnherit(models.Model):
+    _inherit = 'sale.order'
+
+    # partner_id = fields.Many2one(
+    #     'res.partner', string='Customer', readonly=True,
+    #     states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
+    #     required=True, change_default=True, index=True, tracking=1,
+    #     domain="['|',('customer_rank', '>', 0),('supplier_rank', '>', 0)]")
+
+    partner_id = fields.Many2one('res.partner', readonly=True,
+                                 #                                 states={'draft': [('readonly', False)]},
+                                 domain="['|',('customer_rank', '>', 0),('supplier_rank', '>', 0)]",
+                                 string='Partner')
+
+    rif = fields.Char(string="RIF", related='partner_id.vat', store=True, states={'draft': [('readonly', True)]})
+
+    identification_id = fields.Char('Documento de Identidad', related='partner_id.identification_id', size=20,
+                                     store=True, states={'draft': [('readonly', True)]})
+
+    nationality = fields.Selection([
+        ('V', 'Venezolano'),
+        ('E', 'Extranjero'),
+        ('P', 'Pasaporte')], string="Tipo Documento", related='partner_id.nationality', store=True,
+        states={'draft': [('readonly', True)]})
+
+    people_type_company = fields.Selection([
+        ('pjdo', 'PJDO    Persona Jurídica Domiciliada'),
+        ('pjnd', 'PJND    Persona Jurídica No Domiciliada')
+    ], 'Tipo de Persona', related='partner_id.people_type_company')
+
+
+    people_type_individual = fields.Selection([
+        ('pnre', 'PNRE    Persona Natural Residente'),
+        ('pnnr', 'PNNR    Persona Natural No Residente')], 'Tipo de Persona', related='partner_id.people_type_individual')
+
+    company_type = fields.Selection(string='Company Type',
+                                     selection=[('person', 'Individual'), ('company', 'Company')], related='partner_id.company_type')
